e19b732c44a0ce749ef6bd69529a49b8f6f90e7b
toknsiT <toknsiT@outlook.com>
2023-02-13 10:47:42 +0800

    fix: not signature for release

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Noise Recorder',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Colors.blueGrey,
